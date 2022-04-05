c414c45173f088ca61274263034b2515a1c54d79
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-04-05 11:01:47 -0300

    connect database

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDatabase = require("./database/database");
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+connectDatabase();
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
